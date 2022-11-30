cb2a21bc5ab596ba6a3d4de0ae7eea38ff251d41
ErnieBernie10 <arne.boedt1@gmail.com>
2022-11-30 08:34:40 +0100

    Polishing search

--- Dynastic.Architecture/Persistence/DynasticSearchContext.cs
+++ Dynastic.Architecture/Persistence/DynasticSearchContext.cs
@@ -14,13 +14,19 @@ public class DynasticSearchContext : IDynasticSearchContext
         Client = client;
     }
 
-    public async Task<List<T>> SearchWithPagination<T>(string term, int page, int pageSize, CancellationToken cancellationToken = default) where T : ISearchable
+    public async Task<List<T>> SearchWithPagination<T>(string? term, int page, int pageSize, CancellationToken cancellationToken = default) where T : ISearchable
     {
-        return await Client.Search<T>(term)
+        if (string.IsNullOrEmpty(term))
+        {
+            term = "*";
+        }
+
+        return await Client.Search<T>(term, new SearchOptions() {
+                Skip = page - 1,
+                Size = pageSize,
+            })
             .Value
             .GetResultsAsync()
-            .Skip(page - 1)
-            .Take(pageSize)
             .Select(document => document.Document)
             .ToListAsync(cancellationToken);
     }
